b77f29b8da00cf33552270a589c86731df8fcfdb
Niresed <fuadsafarli686@gmail.com>
2023-03-25 21:48:59 +0400

    NGB

---
+++ src/main/java/me/niresed/ngb/NGB.java
@@ -0,0 +1,15 @@
+package me.niresed.ngb;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NGB extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
