25791a7c93eb678e3066ea5b2234aebb94085479
Niresed <fuadsafarli686@gmail.com>
2023-03-30 21:19:25 +0400

    NGB

--- src/main/java/me/niresed/ngb/Utils/NGBUtils.java
+++ src/main/java/me/niresed/ngb/Utils/NGBUtils.java
@@ -5,12 +5,6 @@ import org.bukkit.entity.Player;
 public class NGBUtils {
     public static boolean duration;
 
-    public static void repeat(Player player) throws InterruptedException {
-        int count = 0;
-        while (duration && count < 10000){
-            Thread.sleep(10000);
-            player.sendMessage("Hello");
-            count += 1;
-        }
+    public static void repeat(Player player){
     }
 }
